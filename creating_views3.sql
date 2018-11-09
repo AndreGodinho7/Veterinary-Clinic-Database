16ca8e91aece95731208f307d5e5d3a8615d5e12
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-11-09 19:53:57 +0000

    missing order by

--- creating_views3.sql
+++ creating_views3.sql
@@ -1,4 +1,4 @@
-CREATE VIEW facts_consults AS
-SELECT animal_name, animal_vat, dd.date_timestamp AS timestamp, COUNT(DISTINCT num) AS num_procedures, COUNT(DISTINCT name_med, dosage, lab) AS num_medications
-FROM (dim_animal da NATURAL JOIN animal) NATURAL join (dim_date NATURAL JOIN consult) NATURAL LEFT OUTER JOIN procedures NATURAL LEFT OUTER JOIN prescription 
-GROUP BY timestamp, animal_name;
+SELECT animal_name AS name, animal_vat AS vat, dd.date_timestamp AS timestamp, COUNT(distinct num) AS num_procedures, COUNT(distinct name_med, dosage, lab) AS num_medications
+FROM (dim_animal da NATURAL JOIN animal) NATURAL JOIN (dim_date dd NATURAL JOIN consult) NATURAL LEFT OUTER JOIN procedures NATURAL LEFT OUTER JOIN prescription 
+GROUP BY timestamp, name
+ORDER BY name;
