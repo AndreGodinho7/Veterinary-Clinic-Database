98d2bbafa71a9e584348904a574160cd4aee55a8
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-11-09 11:50:29 +0000

    query6 bem feita

--- query6.sql
+++ query6.sql
@@ -1,10 +1,12 @@
 SELECT
-count(pa.date_timestamp)/count(c.date_timestamp) AS participations_average,
-count(p.date_timestamp)/count(c.date_timestamp) AS procedures__average,
-count(cd.date_timestamp)/count(c.date_timestamp) AS consult_diagnosis_average,
-count(pres.date_timestamp)/count(c.date_timestamp) AS prescriptions_average
+COUNT(distinct pa.name, pa.VAT_owner, pa.date_timestamp, pa.VAT_assistant)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS participations_average,
+COUNT(distinct p.name, p.VAT_owner, p.date_timestamp, p.num)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS procedures__average,
+COUNT(distinct cd.code, cd.name, cd.VAT_owner, cd.date_timestamp)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS consult_diagnosis_average,
+COUNT(distinct pres.code, pres.name, pres.VAT_owner, pres.date_timestamp, pres.name_med, pres.lab, pres.dosage)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS prescriptions_average
 FROM
 consult c NATURAL LEFT OUTER JOIN participation pa NATURAL LEFT OUTER JOIN procedures p
 NATURAL LEFT OUTER JOIN consult_diagnosis cd NATURAL LEFT OUTER JOIN prescription pres
 WHERE
 year(c.date_timestamp) = 2017;
+
+
