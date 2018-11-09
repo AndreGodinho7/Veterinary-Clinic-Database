ddbc2f9e68d321f0c4617fc6d82baa687ffac0a1
diogoegrodrigues <diogoegrodrigues@gmail.com>
2018-11-09 19:50:32 +0000

    acho que é desta

--- query7.sql
+++ query7.sql
@@ -3,6 +3,6 @@ FROM ((((consult_diagnosis cd NATURAL JOIN animal AS a) INNER JOIN species s ON 
 WHERE gs.name2 = 'dog'
 GROUP BY a.species_name, dc.name
 HAVING COUNT(*) >= ALL(SELECT count(*)
-	                   FROM ((((consult_diagnosis cd1 NATURAL JOIN animal AS a1) INNER JOIN species s1 ON (a1.species_name = s1.name)) INNER JOIN generalization_species gs1 ON (s1.name = gs1.name1)) INNER JOIN diagnosis_code dc1 ON (cd1.code = dc1.code))
+                       FROM ((((consult_diagnosis cd1 NATURAL JOIN animal AS a1) INNER JOIN species s1 ON (a1.species_name = s1.name)) INNER JOIN generalization_species gs1 ON (s1.name = gs1.name1)) INNER JOIN diagnosis_code dc1 ON (cd1.code = dc1.code))
 	                   WHERE a.species_name = a1.species_name
 	                   GROUP BY dc1.name);
--- query9.sql
+++ query9.sql
@@ -1,6 +1,6 @@
 SELECT distinct p.name AS client_name, p.address_street AS street, p.address_city AS city, p.address_zip AS zip 
 FROM person p INNER JOIN animal a  on (p.VAT = a.VAT)
 WHERE p.VAT NOT IN (SELECT p.VAT
-	                FROM person p, animal a 
-	                WHERE p.VAT = a.VAT
-	                AND (species_name NOT LIKE '%bird%'));
+                    FROM person p, animal a 
+                    WHERE p.VAT = a.VAT
+                    AND (species_name NOT LIKE '%bird%'));
