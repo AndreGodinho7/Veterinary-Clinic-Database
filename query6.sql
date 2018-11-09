2d89a841b4c75d6e8c620ea5f3e0a1448fa9173a
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-11-09 12:50:10 +0000

    populating for query2

--- query6.sql
+++ query6.sql
@@ -4,9 +4,8 @@ COUNT(distinct p.name, p.VAT_owner, p.date_timestamp, p.num)/COUNT(distinct c.na
 COUNT(distinct cd.code, cd.name, cd.VAT_owner, cd.date_timestamp)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS consult_diagnosis_average,
 COUNT(distinct pres.code, pres.name, pres.VAT_owner, pres.date_timestamp, pres.name_med, pres.lab, pres.dosage)/COUNT(distinct c.name, c.VAT_owner, c.date_timestamp) AS prescriptions_average
 FROM
-consult c NATURAL LEFT OUTER JOIN participation pa NATURAL LEFT OUTER JOIN procedures p
-NATURAL LEFT OUTER JOIN consult_diagnosis cd NATURAL LEFT OUTER JOIN prescription pres
+consult c NATURAL LEFT OUTER JOIN participation pa 
+NATURAL LEFT OUTER JOIN procedures p NATURAL LEFT OUTER JOIN
+consult_diagnosis cd NATURAL LEFT OUTER JOIN prescription pres
 WHERE
 year(c.date_timestamp) = 2017;
-
-
