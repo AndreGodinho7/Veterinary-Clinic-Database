7da0fd273d54a2a35b680ecab4ba5a07b14ad256
diogoegrodrigues <diogoegrodrigues@gmail.com>
2018-11-09 01:56:39 +0000

    tao todas, promenores finais

--- query5.sql
+++ query5.sql
@@ -1,4 +1,4 @@
-select diagnosis_code.name as possible_diagnosis, count(distinct name_med) as number_of_distinct_medication_name
-from (consult_diagnosis natural left outer join prescription) inner join diagnosis_code on (consult_diagnosis.code = diagnosis_code.code)
-group by diagnosis_code.name
-order by count(distinct name_med);
+SELECT diagnosis_code.name AS possible_diagnosis, COUNT(DISTINCT name_med) AS number_of_distinct_medication_name
+FROM (consult_diagnosis NATURAL LEFT OUTER JOIN prescription) INNER JOIN diagnosis_code ON (consult_diagnosis.code = diagnosis_code.code)
+GROUP BY diagnosis_code.name
+ORDER BY COUNT(DISTINCT name_med);
