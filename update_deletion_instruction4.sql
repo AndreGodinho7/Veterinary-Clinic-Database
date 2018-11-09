e7d1582d7b628566d69b08cc09c5710e73e658ac
irmaoshiva <rsmoreira.pedro@gmail.com>
2018-11-09 00:23:33 +0000

    query6 and update4 ident

--- update_deletion_instruction4.sql
+++ update_deletion_instruction4.sql
@@ -1,11 +1,11 @@
 #select code from diagnosis_code where name = 'kidney failure';
 
-insert into diagnosis_code values ('AAHA-15', 'end-stage renal disease');
+INSERT INTO diagnosis_code VALUES ('AAHA-15', 'end-stage renal disease');
 
-update 
-consult_diagnosis cd natural join test_procedure natural join produced_indicator, diagnosis_code dc
-set cd.code = dc.code, cd.name = cd.name, cd.VAT_owner = cd.VAT_owner, cd.date_timestamp = cd.date_timestamp
-where 
-cd.code in (select code from diagnosis_code where name = 'kidney failure') and
-dc.name = 'end-stage renal disease' and 
-type = 'Blood' and indicator_name = 'creatine level' and value > 1.0;
+UPDATE
+consult_diagnosis cd NATURAL JOIN test_procedure NATURAL JOIN produced_indicator, diagnosis_code dc
+SET cd.code = dc.code, cd.name = cd.name, cd.VAT_owner = cd.VAT_owner, cd.date_timestamp = cd.date_timestamp
+WHERE 
+cd.code IN (SELECT code FROM diagnosis_code WHERE name = 'kidney failure') AND
+dc.name = 'end-stage renal disease' AND
+type = 'Blood' AND indicator_name = 'creatine level' AND value > 1.0;
